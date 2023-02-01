777aae01105cf623f30d1d17ecb7783dd79c00a4
rprincya <rebeccaprincy23@gmail.com>
2023-02-01 15:07:51 +0530

    Prince Work

--- src/org/git/GITFirstPrgm.java
+++ src/org/git/GITFirstPrgm.java
@@ -3,11 +3,11 @@ package org.git;
 public class GITFirstPrgm {
 
 	public static void main(String[] args) {
-	
+
 		String s = "Prince";
 		System.out.println(s);
-
-
+		boolean empty = s.isEmpty();
+		System.out.println(empty);
 	}
 
 }
